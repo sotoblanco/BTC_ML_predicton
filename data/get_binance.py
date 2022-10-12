d95976111322a2feafd20217db0f09cff7966eeb
Pastor <sotoblanco263542@gmail.com>
2022-10-12 08:12:15 -0400

    importer data

--- data/get_binance.py
+++ data/get_binance.py
@@ -4,6 +4,7 @@ import requests                    # for "get" request to API
 import json                        # parse json into a list
 import pandas as pd                # working with data frames
 import datetime as dt              # working with dates
+import os.path
 #import os
 #import time
 #from threading import Thread
@@ -40,32 +41,31 @@ def get_binance_bars(symbol, interval, startTime, endTime):
  
     return df
 
-import os.path
+def store_data():
+  if not os.path.isfile('data/BTCUSDT_historical.csv'):
+      print ("File not exist")
+      start_time = dt.datetime(2017, 6, 17)
+      df = pd.DataFrame()
 
-if not os.path.isfile('BTCUSDT_historical.csv'):
-    print ("File not exist")
-    start_time = dt.datetime(2017, 6, 17)
-    df = pd.DataFrame()
+  else:
+      print ("File exist")
+      # read the data previously store in your directory
+      df = pd.read_csv("data/BTCUSDT_historical.csv", index_col="Unnamed: 0", parse_dates=True)
+      # get the last index value
+      start_time = df.index[-5]
 
-else:
-    print ("File exist")
-    # read the data previously store in your directory
-    df = pd.read_csv("BTCUSDT_historical.csv", index_col="Unnamed: 0", parse_dates=True)
-    # get the last index value
-    start_time = df.index[-5]
+  now_time = dt.datetime.now()
 
-now_time = dt.datetime.now()
+  while True:
+    next = start_time + pd.to_timedelta(1000, unit="h")
+    btc = get_binance_bars("BTCUSDT", "1h", start_time, now_time)
+    print(btc.index[-1])
+    df = pd.concat([df, btc])
+    
+    if btc.index[-1].date() == now_time.date():
+      break
+    start_time = btc.index[-1]
 
-while True:
-  next = start_time + pd.to_timedelta(1000, unit="h")
-  btc = get_binance_bars("BTCUSDT", "1h", start_time, now_time)
-  print(btc.index[-1])
-  df = pd.concat([df, btc])
-  
-  if btc.index[-1].date() == now_time.date():
-    break
-  start_time = btc.index[-1]
-
-df.sort_index(inplace=True)
-df = df[~df.index.duplicated(keep='last')]
-df.to_csv("BTCUSDT_historical.csv")
+  df.sort_index(inplace=True)
+  df = df[~df.index.duplicated(keep='last')]
+  df.to_csv("data/BTCUSDT_historical.csv")
---
+++ main.py
@@ -0,0 +1,5 @@
+from data.get_binance import *
+from data.feature_creation import *
+
+store_data()
+feature_engineering()
